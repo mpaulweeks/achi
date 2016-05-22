4683e9ca4bd45212bd3eeeb642d059d7a6a0f057
mpaulweeks <mpaulweeks@gmail.com>
2016-05-22 17:40:38 -0500

    add ai switch, sample forced victory

--- achi.js
+++ achi.js
@@ -1,8 +1,32 @@
 function game(){
 
+read_url_param = function(param_name, as_list){
+    as_list = as_list || false;
+    var vars = {};
+    var q = document.URL.split('?')[1];
+    if(q != undefined){
+        q = q.split('&');
+        for(var i = 0; i < q.length; i++){
+            var param = q[i].split('=');
+            var name = param[0];
+            var value = param[1];
+            vars[name] = vars[name] || [];
+            vars[name].push(value);
+        }
+    }
+    if (vars.hasOwnProperty(param_name)){
+        if (vars[param_name].length == 1 && !as_list){
+            return vars[param_name][0];
+        }
+        return vars[param_name];
+    }
+    return null;
+};
+
 //constants
 var message_query = '#message';
 var players = ['red','blue'];
+var ai_on = false;
 var current_id = 1;
 var current = players[current_id];
 var winningCoords = [
@@ -161,6 +185,7 @@ function action(coord){
 }
 
 function startGame(){
+	ai_on = Boolean(read_url_param('ai'))
 	setup = true;
 	gameOver = false;
 	stonesInHand = 8;
@@ -171,6 +196,21 @@ function startGame(){
 	];
 	switchTurn();
 	draw();
+
+	// practice forcing win
+	if (ai_on){
+		action(pair(1,1));
+		action(pair(0,0));
+		action(pair(2,2));
+		action(pair(2,0));
+		action(pair(1,0));
+		action(pair(1,2));
+		action(pair(0,2));
+		action(pair(0,1));
+		action(pair(2,2));
+		action(pair(1,2));
+		action(pair(0,2));
+	}
 }
 
 $('.block').on('click', function (){
